1a393ceb3a5374821daafb57ad6826eacfd841bc
Felippe017 <felippecorrea2010@gmail.com>
2021-11-07 17:07:49 -0300

    criando API viaCep: adicionando app.use de cep no index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const fibonacciRouter = require('./src/routers/fibonacciRouter');
+const cepRouter = require('./src/routers/viaCepRouter');
 
 const app = express();
 const { PORT } = process.env;
@@ -10,6 +11,7 @@ const { PORT } = process.env;
 app.use(bodyParser.json());
 
 app.use('/fibonacci', fibonacciRouter);
+app.use('/cep', cepRouter);
 
 app.listen(PORT, () => {
   console.log(`Ouvindo porta ${PORT}`);
